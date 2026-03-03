4071fc4b86dc5e3fc02a33a42a818e233ae42518
DulanjanaLakshan <dulanjana20013@gmail.com>
2026-03-03 15:17:02 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(({mode}) => {
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+    preview: {
+      host: true,
+      port: 3000,
+      allowedHosts: ["health-medical-insurance.cypsolabs.com","www.health-medical-insurance.cypsolabs.com"],
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
